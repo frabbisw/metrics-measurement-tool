5f06912ec42f6cee9c79e26cf313937b70f2bb6e
frabbisw <frduiit@gmail.com>
2018-09-24 00:51:24 +0600

    add csv output

--- src/main/java/support/MetricsTool.java
+++ src/main/java/support/MetricsTool.java
@@ -5,20 +5,23 @@ import collect_classes.ClassFinder;
 import collect_classes.FileExplorer;
 import com.github.javaparser.ast.CompilationUnit;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Map;
-import java.util.Set;
 
 public class MetricsTool {
     String projectPath;
+    String outputPath;
     ArrayList<ClassManager> classManagers;
     CallGraphMetrics callGraphMetrics;
 
-    public MetricsTool(String projectPath) {
+    public MetricsTool(String projectPath, String outputPath) {
         classManagers = new ArrayList<>();
         callGraphMetrics = new CallGraphMetrics();
 
         this.projectPath = projectPath;
+        this.outputPath = outputPath;
+
         ClassFinder.setProjectPath(projectPath);
         FileExplorer explorer = ClassFinder.getClassExplorer();
 
@@ -55,17 +58,41 @@ public class MetricsTool {
         callGraphMetrics.generateMethodCallGraph();
     }
 
+    public void printCSV()
+    {
+        String res="";
+        for(ClassManager classManager : classManagers)
+        {
+            res+=(classManager.getMyFullName()+",");
+            res+=(classManager.getLineOfCodes()+",");
+            res+=(classManager.getLineOfComments()+",");
+            res+=(classManager.getCoupling()+",");
+            res+=(classManager.getLackOfCohesion()+",");
+            res+=(classManager.getResponseOfClass()+",");
+            res+=(classManager.getWeightedMethodCount());
+            res+="\n";
+        }
+
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(outputPath));
+            writer.write(res);
+            writer.close();
+            System.out.println(res);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
     public void printAll()
     {
         for(ClassManager classManager : classManagers)
         {
-            System.out.println(classManager.getMyFullName());
-            System.out.println(classManager.getLineOfCodes());
-            System.out.println(classManager.getLineOfComments());
-            System.out.println(classManager.getCoupling());
-            System.out.println(classManager.getLackOfCohesion());
-            System.out.println(classManager.getResponseOfClass());
-            System.out.println(classManager.getWeightedMethodCount());
+            System.out.print(classManager.getMyFullName()+",");
+            System.out.print(classManager.getLineOfCodes()+",");
+            System.out.print(classManager.getLineOfComments()+",");
+            System.out.print(classManager.getCoupling()+",");
+            System.out.print(classManager.getLackOfCohesion()+",");
+            System.out.print(classManager.getResponseOfClass()+",");
+            System.out.print(classManager.getWeightedMethodCount());
             System.out.println();
         }
     }
--- src/main/java/support/TestTool.java
+++ src/main/java/support/TestTool.java
@@ -5,8 +5,8 @@ public class TestTool {
     {
         double time = System.nanoTime();
 
-        MetricsTool tool = new MetricsTool("/home/rabbi/dl4j/soft_metrics");
-        tool.printAll();
+        MetricsTool tool = new MetricsTool("/home/rabbi/dl4j/soft_metrics", "/home/rabbi/Desktop/metricsOutput.csv");
+        tool.printCSV();
 
         System.out.println((System.nanoTime()-time)/1000000);
     }
